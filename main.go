10f861b5fd4afb2ec05027fa6831ee06eeebfc31
Владислав Горбунов <vladgorr1262@gmail.com>
2023-03-21 12:07:13 +0300

    update main

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Go!")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe("localhost:8080", nil)
+}
 func main() {
-	fmt.Println("Hellow world!")
+	handleRequest()
 }
